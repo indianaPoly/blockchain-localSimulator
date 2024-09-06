930aea654b81430562da8903b87178878aa36221
indianaPoly <hyunrim03@naver.com>
2024-09-06 11:40:19 +0900

    style: gofmt 적용

--- block.go
+++ block.go
@@ -8,21 +8,23 @@ import (
 )
 
 type Block struct {
-	Index int
-	Timestamp string
-	Data string
+	Index        int
+	Timestamp    string
+	Data         string
 	PreviousHash string
-	Hash string
+	Hash         string
 }
 
+// * : 메모리 주소에 저장된 값에 접근
+// & : 주소를 나탬
 func NewBlock(index int, data string, previousHash string) *Block {
 	timestamp := time.Now().String()
 	block := &Block{
-		Index: index,
-		Timestamp: timestamp,
-		Data: data,
+		Index:        index,
+		Timestamp:    timestamp,
+		Data:         data,
 		PreviousHash: previousHash,
-		Hash: "",
+		Hash:         "",
 	}
 	block.Hash = block.calculateHash()
 	return block
