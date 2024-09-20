d2a8d27b590ab68622d6693e66add8c9c24da98a
indianaPoly <hyunrim03@naver.com>
2024-09-20 09:37:38 +0900

    feat: create transaction hash

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,11 +1,31 @@
 package utils
 
 import (
+	"blockchain-simulator/pkg/transaction"
 	"blockchain-simulator/pkg/types"
+	"crypto/ecdsa"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"strconv"
 )
+
+func SerializePublicKey(pubKey ecdsa.PublicKey) string {
+	return fmt.Sprintf("%x%x", pubKey.X, pubKey.Y)
+}
+
+func CalculateTransactionHash(tx transaction.Transaction) string {
+	fromKey := SerializePublicKey(tx.From)
+	toKey := SerializePublicKey(tx.To)
+
+	data := fmt.Sprintf("%s:%s:%s:%d:%d", tx.ID, fromKey, toKey, tx.Amount, tx.Gas)
+
+	hash := sha256.New()
+	hash.Write([]byte(data))
+	hashBytes := hash.Sum(nil)
+
+	return hex.EncodeToString(hashBytes)
+}
 
 func CalculateHash(b *types.Block) string {
 	record := strconv.Itoa(b.Index) + b.Timestamp + b.Data + b.PreviousHash
