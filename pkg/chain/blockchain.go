de1567736918ee9662686e859895fb26ea91d139
indianaPoly <hyunrim03@naver.com>
2024-09-20 09:38:54 +0900

    feat: tx 서명 관련 함수 추가

--- pkg/chain/blockchain.go
+++ pkg/chain/blockchain.go
@@ -5,7 +5,11 @@ import (
 	"blockchain-simulator/pkg/block"
 	"blockchain-simulator/pkg/transaction"
 	"blockchain-simulator/pkg/types"
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"errors"
 	"fmt"
+	"math/big"
 	"math/rand"
 	"time"
 )
@@ -15,9 +19,12 @@ type Blockchain struct {
 	Blocks         []*types.Block
 	Validators     map[string]int
 	memPool        []transaction.Transaction
-	BlockGasLimit  int           // 블록 가스 한도
-	TransactionGas int           // 트랜잭션당 가스 소비량
-	MiningInterval time.Duration // 블록 생성 주기
+	BlockGasLimit  int                 // 블록 가스 한도
+	TransactionGas int                 // 트랜잭션당 가스 소비량
+	MiningInterval time.Duration       // 블록 생성 주기
+	balances       map[string]*big.Int // 발신자 주소를 키로 사용하여 잔액을 저장
+	ValidatorRewards map[string]float64
+	RewardPerBlock float64
 }
 
 // 새로운 체인을 만드는 함수
@@ -34,9 +41,98 @@ func NewBlockchain(blockGasLimit, transactionGas int, miningInterval time.Durati
 	return bc
 }
 
+// 올바른 서명이 되었는지를 확인하는 코드
+func (bc *Blockchain) verifySignature(tx transaction.Transaction) bool {
+	// 거래의 메시지를 추출
+	msg := tx.Message()
+	// 메시지의 해시값을 생성
+	hash := sha256.Sum256(msg)
+
+	pubKey := &tx.From
+	r := new(big.Int).SetBytes(tx.Signature[:len(tx.Signature)/2])
+	s := new(big.Int).SetBytes(tx.Signature[len(tx.Signature)/2:])
+
+	return ecdsa.Verify(pubKey, hash[:], r, s)
+}
+
+// r금액 설정
+func (bc *Blockchain) setBalance(address string, amount *big.Int) {
+	bc.balances[address] = amount
+}
+
+// 금액 가져오기
+func (bc *Blockchain) getBalance(address string) *big.Int {
+	balance, exists := bc.balances[address]
+	if !exists {
+		return big.NewInt(0)
+	}
+	return balance
+}
+
+// 잔액이 충분한지 확인하는 함수
+func (bc *Blockchain) hasSufficientBalance(tx transaction.Transaction) bool {
+	balance := bc.balances[tx.From.X.String()]
+	
+	amount := new(big.Int).SetInt64(int64(tx.Amount))
+	gas := new(big.Int).SetInt64(int64(tx.Gas))
+
+	total := new(big.Int).Add(amount, gas)
+
+	if balance.Cmp(total) >= 0 {
+		return true
+	}
+
+	return false
+}
+
+// 중복 거래를 확인하는 함수
+// 동일한 트랜젝션 아이디가 존재하면 true 반환
+func (bc *Blockchain) isDuplicate(tx transaction.Transaction) bool {
+	for _, existingTx := range bc.memPool {
+		if existingTx.ID == tx.ID {
+			return true
+		}
+	}
+
+	return false
+}
+
+// 트랜젝션을 확인하는 함수
+func (bc *Blockchain) validateTransaction(tx transaction.Transaction) error {
+	if tx.Amount <= 0 {
+		return errors.New("보내는 금액이 0보다 작습니다")
+	}
+
+	if tx.Gas < 1 {
+		return errors.New("가스값이 최소 단위보다 작습니다.")
+	}
+
+	if !bc.verifySignature(tx) {
+		return errors.New("서명이 올바르지 않습니다.")
+	}
+
+	if !bc.hasSufficientBalance(tx) {
+		return errors.New("insufficient balacnce for tx")
+	}
+
+	if bc.isDuplicate(tx) {
+		return errors.New("tx is a duplicate")
+	}
+
+	return nil
+}
+
 // 트랜젝션을 mempool에 저장
-func (bc *Blockchain) AddTransaction(tx transaction.Transaction, gas int) {
+// 검증하는 로직이 추가적으로 필요할 것으로 판단이 됨.
+func (bc *Blockchain) AddTransaction(tx transaction.Transaction, gas int) error {
+	err := bc.validateTransaction(tx)
+	if err != nil {
+		return err
+	}
+
+	// 유효한 거래에 대해서 mempool에 저장
 	bc.memPool = append(bc.memPool, tx)
+	return nil
 }
 
 // PoS 알고리즘을 활용하여 Validator를 알아냄
@@ -70,7 +166,10 @@ func (bc *Blockchain) startMining() {
 	}
 }
 
+// reward 분배 (구현)
+
 // memPool에서 트랜젝션을 선택하여 블록을 생성하는 함수
+// 여기서 구현이 됨.
 func (bc *Blockchain) CreateBlockFromTransactions(validatorID string) {
 	if len(bc.memPool) == 0 {
 		return
@@ -78,11 +177,17 @@ func (bc *Blockchain) CreateBlockFromTransactions(validatorID string) {
 
 	// 가스 한도에 맞게 트랜잭션 선택
 	var selectedTxs []transaction.Transaction
+	var txHashes []string
 	var totalGas int
+
 	for _, tx := range bc.memPool {
 		if totalGas+bc.TransactionGas > bc.BlockGasLimit {
 			break
 		}
+
+		txHash := utils.CalculateTransactionHash(tx)
+		txHashes = append(txHashes, txHash)
+
 		selectedTxs = append(selectedTxs, tx)
 		totalGas += bc.TransactionGas
 	}
