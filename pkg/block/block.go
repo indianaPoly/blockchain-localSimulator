734fad4fb0566c7cfa35ad0b49d8207cd0c05f2b
indianaPoly <hyunrim03@naver.com>
2024-09-24 14:30:40 +0900

    update: 구조체 변경에 따른 NewBlock func 수정

--- pkg/block/block.go
+++ pkg/block/block.go
@@ -1,26 +1,26 @@
 package block
 
 import (
-	"blockchain-simulator/internal/utils"
+	"blockchain-simulator/pkg/transaction"
 	"blockchain-simulator/pkg/types"
 	"time"
 )
 
 // * : 메모리 주소에 저장된 값에 접근
 // & : 주소를 나탬
-func NewBlock(index int, data string, previousHash string) *types.Block {
+func NewBlock(index int, data string, previousHash string, transactions []transaction.Transaction, merkleRoot string) *types.Block {
 	timestamp := time.Now().String()
 	block := &types.Block{
 		Index:        index,
 		Timestamp:    timestamp,
 		Data:         data,
 		PreviousHash: previousHash,
-		Hash:         "",
+		Transactions: transactions,
+		MerkleRoot:   merkleRoot,
 	}
-	block.Hash = utils.CalculateHash(block)
 	return block
 }
 
 func CreateGenesisBlock() *types.Block {
-	return NewBlock(0, "Genesis Block", "")
+	return NewBlock(0, "Genesis Block", "", []transaction.Transaction{}, "")
 }
