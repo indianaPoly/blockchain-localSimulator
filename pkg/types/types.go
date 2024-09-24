77c2b268f8415ac9b57270a2e18421856d064e57
indianaPoly <hyunrim03@naver.com>
2024-09-24 14:30:12 +0900

    feat: 블록 구조체 변경

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,9 +1,12 @@
 package types
 
+import "blockchain-simulator/pkg/transaction"
+
 type Block struct {
 	Index        int
 	Timestamp    string
+	Transactions [] transaction.Transaction
 	Data         string
 	PreviousHash string
-	Hash         string
+	MerkleRoot   string
 }
