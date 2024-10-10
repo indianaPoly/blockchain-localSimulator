307139e955e40d94b422493664aad51bb2466e24
indianaPoly <hyunrim03@naver.com>
2024-10-10 15:10:15 +0900

    update: PoS 알고리즘 수정

--- blockchain/validator.go
+++ blockchain/validator.go
@@ -1,22 +1,44 @@
 package blockchain
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
+
+type Validator struct {
+	Stake int
+	Count int
+}
 
 // PoS 알고리즘을 활용하여 Validator를 알아냄
 func (bc *Blockchain) SelectValidator() string {
 	totalStake := 0
 
-	for _, stake := range bc.Validators {
-		totalStake += stake
+	// 전체 stake를 계산
+	for _, validator := range bc.Validators {
+		// validator들 중에서 stake가 조금이라도 있는 사람들한테 기본적인 가중치를 제공하여
+		// validator가 될 수 있게끔
+		if validator.Stake > 0 {
+			totalStake += validator.Stake
+		}
 	}
 
-	randValue := rand.Intn(totalStake)
-	cumulaticeStake := 0
+	// 로컬 랜덤 생성
+	rng := rand.New(rand.NewSource(time.Now().UnixMicro()))
+	randomFloat := rng.Float64()
 
-	for validator, stake := range bc.Validators {
-		cumulaticeStake += stake
-		if randValue < cumulaticeStake {
-			return validator
+	// 랜덤 값에 따른 검증자 선택
+	cumulativeProbability := 0.0
+
+	for validatorID, validator := range bc.Validators {
+		// 각 검증자의 확률 비율 계산
+		probability := float64(validator.Stake) / float64(totalStake)
+		cumulativeProbability += probability
+
+		// 랜덤 값이 해당 검증자의 확률 구간에 속하면 해당 검증자를 선택
+		if randomFloat <= cumulativeProbability {
+			validator.Count++
+			return validatorID
 		}
 	}
 
