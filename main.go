4d91ebbdf68e18500b083c4f0e57b6c7bab7f0c4
indianaPoly <hyunrim03@naver.com>
2024-10-10 15:09:14 +0900

    delete: 필요없는 내용 삭제

--- main.go
+++ main.go
@@ -1,26 +1,6 @@
 package main
 
-import (
-	"fmt"
-
-	"blockchain-simulator/pkg/chain"
-)
-
 func main() {
-	// 새로운 블록체인 생성
-	blockchain := chain.NewBlockchain()
-
-	// 블록 추가
-	blockchain.AddBlock("첫 번째 트랜잭션")
-	blockchain.AddBlock("두 번째 트랜잭션")
-
-	// 블록체인 출력
-	for _, block := range blockchain.Blocks {
-		fmt.Printf("Index: %d\n", block.Index)
-		fmt.Printf("Timestamp: %s\n", block.Timestamp)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Previous Hash: %s\n", block.PreviousHash)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Println()
-	}
+	// 새로운 블록체인 생성 (매개변수에 들어가는 갑승ㄹ 고정해서 진행을 해야할 것으로 판단)
+	// blockchain := blockchain.NewBlockchain()
 }
